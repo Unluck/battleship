708870ffadb9a285818b90f4b72d0e716de624e5
Юрий Кириллов <yuvkirillov@edu.hse.ru>
2016-12-10 16:57:33 +0300

    Some UI

---
+++ BattleShip.UI/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using BattleShip.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BattleShip.UI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //MusicSettings.BackgroundMusic();
+        }
+
+        private void buttonPlay_Click(object sender, RoutedEventArgs e)
+        {
+            var UserName = textBoxUserName.Text;
+            ModeSelection ModeSelection = new ModeSelection();
+            ModeSelection.ShowDialog();
+        }
+    }
+}
---
+++ BattleShip.UI/ModeSelection.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BattleShip.UI
+{
+    /// <summary>
+    /// Логика взаимодействия для ModeSelection.xaml
+    /// </summary>
+    public partial class ModeSelection : Window
+    {
+        public ModeSelection()
+        {
+            InitializeComponent();
+        }
+
+        private void button1Player_Click(object sender, RoutedEventArgs e)
+        {
+            OnePlayer OnePlayer = new OnePlayer();
+            OnePlayer.ShowDialog();
+            
+        }
+
+        private void buttonBack_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow MainWindow = new MainWindow();
+            MainWindow.ShowDialog();
+        }
+    }
+}
