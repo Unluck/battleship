f11a709a9abab38adee83c25ff19d85b38b77f51
Юрий Кириллов <yuvkirillov@edu.hse.ru>
2016-12-10 22:09:35 +0300

    Added field for ship placement

---
+++ BattleShip.UI/ShipPlacementWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BattleShip.UI
+{
+    /// <summary>
+    /// Логика взаимодействия для ShipPlacementWindow.xaml
+    /// </summary>
+    public partial class ShipPlacementWindow : Window
+    {
+        public ShipPlacementWindow()
+        {
+            
+        }
+
+        private void canvasField_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point position = e.GetPosition(canvasField);
+            Point p = new Point((int)(position.X / (canvasField.ActualWidth / 10)), (int)(position.Y / (canvasField.ActualHeight / 10)));
+        }
+    }
+}
