ec1c129406dc037f33b3f42c8f5557f722462fa3
ProgProjUs <mitrofanov197@gmail.com>
2016-12-15 21:11:18 +0300

    final randomcomp shipplacement logic

--- BattleShip.Logic/ComputerLogic.cs
+++ BattleShip.Logic/ComputerLogic.cs
@@ -9,7 +9,6 @@ namespace BattleShip.Logic
     public class ComputerLogic
     {
         public List<Ship> enemyShip = new List<Ship>();
-        //int[,] shipField = new int[10,10];
         int[,] localShip = new int[10, 10];
 
 
@@ -30,28 +29,28 @@ namespace BattleShip.Logic
                             var p2 = new Location(p1.x + 1, p1.y);
                             var p3 = new Location(p2.x + 1, p1.y);
                             var p4 = new Location(p3.x + 1, p1.y);
-                            for (int k = 0; k < localShip.GetLength(0); k++)
+                            if (localShip[p1.x, p1.y] == 1)
                             {
-                                for (int l = 0; l < localShip.GetLength(1); l++)
-                                {
-                                    if (localShip[k, l] == 1)
-                                    {
-                                        if (p1.x == k & p1.y == l)
-                                            j = --j;
-                                        if (p2.x == k & p2.y == l)
-                                            j = --j;
-                                        if (p3.x == k & p3.y == l)
-                                            j = --j;
-                                        if (p4.x == k & p4.y == l)
-                                            j = --j;
-                                    }
-
-                                }
+                                j = --j;
+                                continue;
+                            }
+                            if (localShip[p2.x, p2.y] == 1)
+                            {
+                                j = --j;
+                                continue;
+                            }
+                            if (localShip[p3.x, p3.y] == 1)
+                            {
+                                j = --j;
+                                continue;
+                            }
+                            if (localShip[p4.x, p4.y] == 1)
+                            {
+                                j = --j;
+                                continue;
                             }
                             enemyShip.Add(new Ship(i, 0, new Location[] { p1, p2, p3, p4 }));
                             CheckUniqueShip(new Location[] { p1, p2, p3, p4 });
-                            
-                            
                         }
                         if (position == 1)
                         {
@@ -59,183 +58,145 @@ namespace BattleShip.Logic
                             var p2 = new Location(p1.x, p1.y + 1);
                             var p3 = new Location(p1.x, p2.y + 1);
                             var p4 = new Location(p1.x, p3.y + 1);
-                            for (int k = 0; k < localShip.GetLength(0); k++)
+                            if (localShip[p1.x, p1.y] == 1)
                             {
-                                for (int l = 0; l < localShip.GetLength(1); l++)
-                                {
-                                    if (localShip[k, l] == 1)
-                                    {
-                                        if (p1.x == k & p1.y == l)
-                                            j = --j;
-                                        if (p2.x == k & p2.y == l)
-                                            j = --j;
-                                        if (p3.x == k & p3.y == l)
-                                            j = --j;
-                                        if (p4.x == k & p4.y == l)
-                                            j = --j;
-                                    }
-
-                                }
+                                j = --j;
+                                continue;
+                            }
+                            if (localShip[p2.x, p2.y] == 1)
+                            {
+                                j = --j;
+                                continue;
+                            }
+                            if (localShip[p3.x, p3.y] == 1)
+                            {
+                                j = --j;
+                                continue;
+                            }
+                            if (localShip[p4.x, p4.y] == 1)
+                            {
+                                j = --j;
+                                continue;
                             }
                             enemyShip.Add(new Ship(i, 0, new Location[] { p1, p2, p3, p4 }));
                             CheckUniqueShip(new Location[] { p1, p2, p3, p4 });
                         }
                     }
-
                 }
-               /* if (i==1)
+                if (i == 3)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        int position = r.Next(0, 2);
+                        if (position == 0)
+                        {
+                            var p1 = new Location(r.Next(0, 8), r.Next(0, 10));
+                            var p2 = new Location(p1.x + 1, p1.y);
+                            var p3 = new Location(p2.x + 1, p1.y);
+                            if (localShip[p1.x, p1.y] == 1)
+                            {
+                                j = --j;
+                                continue;
+                            }
+                            if (localShip[p2.x, p2.y] == 1)
+                            {
+                                j = --j;
+                                continue;
+                            }
+                            if (localShip[p3.x, p3.y] == 1)
+                            {
+                                j = --j;
+                                continue;
+                            }
+                            enemyShip.Add(new Ship(i, 0, new Location[] { p1, p2, p3 }));
+                            CheckUniqueShip(new Location[] { p1, p2, p3 });
+                        }
+                        if (position == 1)
+                        {
+                            var p1 = new Location(r.Next(0, 10), r.Next(0, 8));
+                            var p2 = new Location(p1.x, p1.y + 1);
+                            var p3 = new Location(p1.x, p2.y + 1);
+                            if (localShip[p1.x, p1.y] == 1)
+                            {
+                                j = --j;
+                                continue;
+                            }
+                            if (localShip[p2.x, p2.y] == 1)
+                            {
+                                j = --j;
+                                continue;
+                            }
+                            if (localShip[p3.x, p3.y] == 1)
+                            {
+                                j = --j;
+                                continue;
+                            }
+                            enemyShip.Add(new Ship(i, 0, new Location[] { p1, p2, p3 }));
+                            CheckUniqueShip(new Location[] { p1, p2, p3 });
+                        }
+                    }
+                }
+                if (i == 2)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        int position = r.Next(0, 2);
+                        if (position == 0)
+                        {
+                            var p1 = new Location(r.Next(0, 9), r.Next(0, 10));
+                            var p2 = new Location(p1.x + 1, p1.y);
+                            if (localShip[p1.x, p1.y] == 1)
+                            {
+                                j = --j;
+                                continue;
+                            }
+                            if (localShip[p2.x, p2.y] == 1)
+                            {
+                                j = --j;
+                                continue;
+                            }
+                            enemyShip.Add(new Ship(i, 0, new Location[] { p1, p2 }));
+                            CheckUniqueShip(new Location[] { p1, p2 });
+                        }
+                        if (position == 1)
+                        {
+                            {
+                                var p1 = new Location(r.Next(0, 10), r.Next(0, 9));
+                                var p2 = new Location(p1.x, p1.y + 1);
+                                if (localShip[p1.x, p1.y] == 1)
+                                {
+                                    j = --j;
+                                    continue;
+                                }
+                                if (localShip[p2.x, p2.y] == 1)
+                                {
+                                    j = --j;
+                                    continue;
+                                }
+                                enemyShip.Add(new Ship(i, 0, new Location[] { p1, p2 }));
+                                CheckUniqueShip(new Location[] { p1, p2 });
+                            }
+                        }
+                    }
+                }
+                if (i == 1)
                 {
                     for (int j = 0; j < 4; j++)
                     {
-                        
                         var p1 = new Location(r.Next(0, 10), r.Next(0, 10));
-                        for (int k = 0; k < localShip.GetLength(0); k++)
+                        if (localShip[p1.x, p1.y] == 1)
                         {
-                            for (int l = 0; l < localShip.GetLength(1); l++)
-                            {
-                                if (localShip[k, l] == 1)
-                                    if (p1.x == k & p1.y == l)
-                                        j = --j;
-                            }
+                            j = --j;
+                            continue;
                         }
                         enemyShip.Add(new Ship(i, 0, new Location[] { p1 }));
                         CheckUniqueShip(new Location[] { p1 });
                     }
                 }
-                if (i==2)
-                {
-                    for (int j = 0; j < 2; j++)
-                    {
-                        // int position = r.Next(0, 2);
-                        //if (position == 0)
-                        //{
-                        var p1 = new Location(r.Next(0, 9), r.Next(0, 10));
-                        var p2 = new Location(p1.x + 1, p1.y);
-                        for (int k = 0; k < localShip.GetLength(0); k++)
-                        {
-                            for (int l = 0; l < localShip.GetLength(1); l++)
-                            {
-                                if (localShip[k, l] == 1)
-                                {
-                                    if (p1.x == k & p1.y == l)
-                                        j = --j;
-                                    else if (p2.x == k & p2.y == l)
-                                        j = --j;
-                                }
-
-                            }
-                        }
-                        enemyShip.Add(new Ship(i, 0, new Location[] { p1, p2 }));
-                        CheckUniqueShip(new Location[] { p1, p2 });
-
-                    }
-
-
-
-                         }
-                      /*  else
-                       {
-                            var p1 = new Location(r.Next(0, 10), r.Next(0, 9));
-                            var p2 = new Location(p1.x, p1.y + 1);
-                            if (CheckUniqueShip(new Location[] { p1, p2 }))
-                            {
-                                enemyShip.Add(new Ship(i, 0, new Location[] { p1, p2 }));
-                            }
-                            else j = --j;
-                        }
-                    }
-                }*/
-                if (i==3) 
-                {
-                    for (int j = 0; j < 2; j++)
-                    {
-                        int position = r.Next(0, 2);
-                        if (position == 0) 
-                        {
-                            var p1 = new Location(r.Next(0, 8), r.Next(0, 10));
-                            var p2 = new Location(p1.x + 1, p1.y);
-                            var p3 = new Location(p2.x + 1, p1.y);
-                            for (int k = 0; k < localShip.GetLength(0); k++)
-                            {
-                                for (int l = 0; l < localShip.GetLength(1); l++)
-                                {
-                                    if (localShip[k, l] == 1)
-                                    {
-                                        if (p1.x == k & p1.y == l)
-                                            j = --j;
-                                        if (p2.x == k & p2.y == l)
-                                            j = --j;
-                                        if (p3.x == k & p3.y == l)
-                                            j = --j;
-                                    }
-
-                                }
-                            }
-                            enemyShip.Add(new Ship(i, 0, new Location[] { p1, p2, p3 }));
-                            CheckUniqueShip(new Location[] { p1, p2, p3 });
-                        }
-                        else 
-                        {
-                            var p1 = new Location(r.Next(0, 10), r.Next(0, 8));
-                            var p2 = new Location(p1.x, p1.y + 1);
-                            var p3 = new Location(p1.x, p2.y + 1);
-                            for (int k = 0; k < localShip.GetLength(0); k++)
-                            {
-                                for (int l = 0; l < localShip.GetLength(1); l++)
-                                {
-                                    if (localShip[k, l] == 1)
-                                    {
-                                        if (p1.x == k & p1.y == l)
-                                            j = --j;
-                                        if (p2.x == k & p2.y == l)
-                                            j = --j;
-                                        if (p3.x == k & p3.y == l)
-                                            j = --j;
-                                    }
-
-                                }
-                            }
-                            enemyShip.Add(new Ship(i, 0, new Location[] { p1, p2, p3 }));
-                            CheckUniqueShip(new Location[] { p1, p2, p3 });
-                        }
-                    }
-                }
-                /*
-                if(i==4) 
-                {
-                    for (int j = 0; j < 1; j++)
-                    {
-                        int position = r.Next(0, 2);
-                        if (position == 0)
-                        {
-                            var p1 = new Location(r.Next(0, 7), r.Next(0, 10));
-                            var p2 = new Location(p1.x + 1, p1.y);
-                            var p3 = new Location(p2.x + 1, p1.y);
-                            var p4 = new Location(p3.x + 1, p1.y);
-                            if (CheckUniqueShip(new Location[] { p1, p2, p3, p4 }))
-                            {
-                                enemyShip.Add(new Ship(i, 0, new Location[] { p1, p2, p3, p4 }));
-                            }
-                            else j = --j;
-                        }
-                        if (position == 1)
-                        {
-                            var p1 = new Location(r.Next(0, 10), r.Next(0, 7));
-                            var p2 = new Location(p1.x, p1.y + 1);
-                            var p3 = new Location(p1.x, p2.y + 1);
-                            var p4 = new Location(p1.x, p3.y + 1);
-                            if (CheckUniqueShip(new Location[] { p1, p2, p3, p4 }))
-                            {
-                                enemyShip.Add(new Ship(i, 0, new Location[] { p1, p2, p3, p4 }));
-                            }
-                            else j = --j;
-                        }
-                    }
-
-                }*/
             }
         }
-
+                
+           
         private void CheckUniqueShip(Location[] massloc)
         {
             foreach (var s in massloc)
