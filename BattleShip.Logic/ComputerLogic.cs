d5b55ff3a3bf132c06a2ca2199bf3aeab45483e0
ProgProjUs <mitrofanov197@gmail.com>
2016-12-17 16:03:57 +0300

    update

--- BattleShip.Logic/ComputerLogic.cs
+++ BattleShip.Logic/ComputerLogic.cs
@@ -203,6 +203,9 @@ namespace BattleShip.Logic
                     }
                 }
             }
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                    localShip[i, j] = 0;
         }
                 
            
